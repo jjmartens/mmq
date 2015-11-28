28822d49540a5f1f9fbf0bcb93b49a8f9fbdc9a8
Jan <jan-martens@hotmail.com>
2015-11-29 00:08:00 +0100

    added full history

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def channelindex(channel_slug):
     channel = Channel.query.filter_by(slug=channel_slug).first()
     if not channel:
         return jsonify({"msg","notfound"})
-    return render_template('channelindex.html', channel=channel)
+    records = db.session.query(func.count(Record.id).label('aantal'), Video.code.label('code'), Video.title.label('title'), Video.duration.label('duration')).filter(Record.channel_id==channel.id).join(Video).group_by(Video.id).all()
+    return render_template('channelindex.html', channel=channel, records=records)
 
 
 @app.route('/', methods=['GET', 'POST'])
